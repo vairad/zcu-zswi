2239c2cc3fea0932d7d62adf355a6352f936bf82
Thoronir42 <Thoronir42@Gmail.com>
2015-03-30 00:13:58 +0200

    Základní nástřel úvodního komentáře

--- src/arduino/arduino.ino
+++ src/arduino/arduino.ino
@@ -1,34 +1,15 @@
 /*
- *  eHealth sensor platform for Arduino and Raspberry from Cooking-hacks.
+ *  Original example code which this code is based on can be found at: 
+ *  http://www.cooking-hacks.com/documentation/tutorials/ehealth-biometric-sensor-platform-arduino-raspberry-pi-medical
+ * 
+ *  Jauvajs Ino
+ *  Bluetooth relayed loop designed for collecting and sending values from sensors.
+ *  
+ *  Allows selection of individual sensors to scan and send via commands scanned on
+ *  serial port.
  *
- *  Description: "The e-Health Sensor Shield allows Arduino and Raspberry Pi
- *  users to perform biometric and medical applications by using 9 different
- *  sensors: Pulse and Oxygen in Blood Sensor (SPO2), Airflow Sensor (Breathing),
- *  Body Temperature, Electrocardiogram Sensor (ECG), Glucometer, Galvanic Skin
- *  Response Sensor (GSR - Sweating), Blood Pressure (Sphygmomanometer) and
- *  Patient Position (Accelerometer)."
- *
- *  Explanation: This example shows the way to communicate with
- *  the Arduino using ZigBee protocol.
- *
- *  Copyright (C) 2012 Libelium Comunicaciones Distribuidas S.L.
- *  http://www.libelium.com
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Version 2.0
- *  Author: Luis Martin & Ahmad Saad
+ *  Version 0.5
+ *  Author: Stepan Sevcik @ SEJDREm
  */
  
 #include <PinChangeInt.h>
