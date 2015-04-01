cb8349ac847c43a18bf9b20279a5b644bc9df4ad
denitara <denitara@students.zcu.cz>
2015-04-01 17:19:20 +0200

    Drobné opravy překlepů, kterých jsem si všimla v komentářích kódu a odebrání zbytečných duplikátů z materiálů o EKG

--- src/desktop/JauvajsDesktop/core/iminer.h
+++ src/desktop/JauvajsDesktop/core/iminer.h
@@ -13,14 +13,14 @@ public:
 
     /**
      * Metoda, která zaručuje odeslání zprávy směrem do zařízení pro získání dat.
-     * Frmát zpráv viz. GitHub Wiki - Referenční příručka Arduino
+     * Formát zpráv viz GitHub Wiki - Referenční příručka Arduino
      *
      * @param line - řádek zprávy pro zdroj dat
      */
    virtual void sendMessage(string line) = 0;
 
     /**
-     * Metoda, která vrátí poslední zprávu datového zdroje nebo nic, pokud není nmová zpráva k dispozici.
+     * Metoda, která vrátí poslední zprávu datového zdroje nebo nic, pokud není nová zpráva k dispozici.
      *
      * @return řádek poslední zprávy nebo null, pokud není nová zpráva k dispozici.
      */
