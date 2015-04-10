62be9e7c8b93cc9a238d43599ca5ab91cbaacb2c
denitara <denitara@students.zcu.cz>
2015-04-10 15:16:55 +0200

    doplnění transcriberu

--- src/desktop/JauvajsDesktop/analysis/transcriberekg.cpp
+++ src/desktop/JauvajsDesktop/analysis/transcriberekg.cpp
@@ -1,20 +1,30 @@
 #include "transcriberekg.h"
 
-TranscriberEKG::TranscriberEKG() {
+TranscriberEKG::TranscriberEKG(float data[], int size) {
+    this->dataSize = size;
+    setData(data, dataSize);
+}
+
+void TranscriberEKG::setData(float data[], int size) {
+    int i;
+    for(i = 0; i < size; i++) {
+        this->data.push_back(data[i]);
+    }
+}
+
+void TranscriberEKG::transcribeData() {
 
 }
 
-void setData(float data[]) {
-
+<<<<<<< HEAD
+vector<char> TranscriberEKG::getString() {
+    return transcribedData;
 }
-
-void transcribeData() {
-
-}
-
+=======
 //char* getString() {
 
 //}
+>>>>>>> 3dc8e5b5580f10b96d039bbae8c9aa02a4dd0da5
 
 TranscriberEKG::~TranscriberEKG() {
 
--- src/desktop/JauvajsDesktop/analysis/transcriberekg.h
+++ src/desktop/JauvajsDesktop/analysis/transcriberekg.h
@@ -1,20 +1,35 @@
 #ifndef TRANSCRIBEREKG_H
 #define TRANSCRIBEREKG_H
+#include <vector>
+
+using namespace std;
 
 /**
  * Prepisovac krivky EKG na retezec znaku.
- * @brief The transcriberekg class
+ * @brief The TranscriberEKG class
  */
 class TranscriberEKG {
 
 public:
-    TranscriberEKG();
-    void setData(float data[]);
+    explicit TranscriberEKG(float data[], int size);
+    void setData(float data[], int size);
     void transcribeData();
-    char* getString();
+    vector<char> getString();
     ~TranscriberEKG();
+<<<<<<< HEAD
+    vector<float> data;
+    vector<char> transcribedData;
+
+private:
+    int dataSize;
+    float soaring; /* prudke stoupani */
+    float diving; /* prudke klesani */
+    float up; /* stoupani */
+    float down; /* klesani */
+=======
     float data[];
     //char transcribedData[];
+>>>>>>> 3dc8e5b5580f10b96d039bbae8c9aa02a4dd0da5
 };
 
 #endif // TRANSCRIBEREKG_H
