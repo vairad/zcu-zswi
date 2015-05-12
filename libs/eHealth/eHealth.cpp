bb7de8293e834428dc43e5c2d523b8320386920d
Thoronir42 <Thoronir42@Gmail.com>
2015-05-12 16:34:41 +0200

    Korekce konfigurace knihovny týkající se senzoru teploty

--- libs/eHealth/eHealth.cpp
+++ libs/eHealth/eHealth.cpp
@@ -198,7 +198,7 @@
 		float Resistance;  //Resistance of sensor.
 		float ganancia=5.0;
 		float Vcc=3.3;
-		float RefTension=0.4; // Voltage Reference of Wheatstone bridge.
+		float RefTension=3.0; // Voltage Reference of Wheatstone bridge.
 		float Ra=4620.0; //Wheatstone bridge resistance.
 		float Rc=4690.0; //Wheatstone bridge resistance.
 		float Rb=813.0; //Wheatstone bridge resistance.
